ec37dff1924644d5824dd9cd277aeea065a5e41c
qingting.jiao <1259043979@qq.com>
2020-05-29 11:52:46 +0800

    轮播

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import store from './store'
 import axios from 'axios'
 import ElementUI from 'element-ui'
 import VueAxios from 'vue-axios'
-import "./assets/scss/common.scss"
-import "./assets/scss/iconfont.css"
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
+import "./assets/scss/common.scss"
+import "./assets/scss/iconfont.css"
+import "../src/assets/scss/main.scss"
+import "../src/assets/scss/header.scss"
+import "../src/assets/scss/footer.scss"
 import '../node_modules/swiper/css/swiper.min.css'
 
 
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -19,20 +19,20 @@
           <!-- 分页 -->
           <div class="swiper-pagination"
                slot="pagination"></div>
-          <ul class="category-item">
-            <li><a>手机 电话卡 <i class="el-icon-aliqianjinxiayige"></i></a></li>
-            <li><a>电视 盒子 <i class="el-icon-aliqianjinxiayige"></i></a></li>
-            <li><a>笔记本 显示器 平板 <i class="el-icon-aliqianjinxiayige"></i></a></li>
-            <li><a>家电 插线板 <i class="el-icon-aliqianjinxiayige"></i></a></li>
-            <li><a>出行 穿戴<i class="el-icon-aliqianjinxiayige"></i></a></li>
-            <li><a>智能 路由器 <i class="el-icon-aliqianjinxiayige"></i></a></li>
-            <li><a>电源 配件 <i class="el-icon-aliqianjinxiayige"></i></a></li>
-            <li><a>健康 儿童 <i class="el-icon-aliqianjinxiayige"></i></a></li>
-            <li><a>耳机 音箱 <i class="el-icon-aliqianjinxiayige"></i></a></li>
-            <li><a>生活 箱包 <i class="el-icon-aliqianjinxiayige"></i></a></li>
-          </ul>
-          <div class="category-info"></div>
         </swiper>
+        <ul class="category-item">
+          <li><a>手机 电话卡 <i class="el-icon-aliqianjinxiayige"></i></a></li>
+          <li><a>电视 盒子 <i class="el-icon-aliqianjinxiayige"></i></a></li>
+          <li><a>笔记本 显示器 平板 <i class="el-icon-aliqianjinxiayige"></i></a></li>
+          <li><a>家电 插线板 <i class="el-icon-aliqianjinxiayige"></i></a></li>
+          <li><a>出行 穿戴<i class="el-icon-aliqianjinxiayige"></i></a></li>
+          <li><a>智能 路由器 <i class="el-icon-aliqianjinxiayige"></i></a></li>
+          <li><a>电源 配件 <i class="el-icon-aliqianjinxiayige"></i></a></li>
+          <li><a>健康 儿童 <i class="el-icon-aliqianjinxiayige"></i></a></li>
+          <li><a>耳机 音箱 <i class="el-icon-aliqianjinxiayige"></i></a></li>
+          <li><a>生活 箱包 <i class="el-icon-aliqianjinxiayige"></i></a></li>
+        </ul>
+        <div class="category-info"></div>
       </div>
     </main>
 
@@ -64,32 +64,34 @@ export default {
           href: "https://www.mi.com/a/h/15490.html"
         },
         {
-          href: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/7e882fcbdcafc352dde03c741836399b.jpg?thumb=1&w=2452&h=920&f=webp&q=90",
+          imgSrc: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/7e882fcbdcafc352dde03c741836399b.jpg?thumb=1&w=2452&h=920&f=webp&q=90",
           href: "https://www.mi.com/a/h/15490.html"
         },
         {
-          href: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/400f15aedf3b2981c462e8811e88d384.jpg?w=2452&h=920",
-          imgSrc: ""
+          imgSrc: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/400f15aedf3b2981c462e8811e88d384.jpg?w=2452&h=920",
+          href: "https://www.mi.com/a/h/15490.html"
         },
         {
-          href: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/f306ce6727f8166fd97730c5876bfadc.jpg?thumb=1&w=2452&h=920&f=webp&q=90",
-          imgSrc: ""
+          imgSrc: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/f306ce6727f8166fd97730c5876bfadc.jpg?thumb=1&w=2452&h=920&f=webp&q=90",
+          href: "https://www.mi.com/a/h/15490.html"
         },
         {
-          href: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/1b5bc9a947319a1d7424cb209c632b7d.jpg?thumb=1&w=2452&h=920&f=webp&q=90",
-          imgSrc: ""
+          imgSrc: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/1b5bc9a947319a1d7424cb209c632b7d.jpg?thumb=1&w=2452&h=920&f=webp&q=90",
+          href: "https://www.mi.com/a/h/15490.html"
         }
       ],
       swiperOption: {
         notNextTick: true,
         loop: true,
         initialSlide: 0,
-        autoplay: {
-          delay: 1500,
-          stopOnLastSlide: false,
-          disableOnInteraction: false
-        },
+        autoplay: true,
         speed: 800,
+
+        //左右点击
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
 
         //分页器设置 
         pagination: {
@@ -107,6 +109,7 @@ export default {
     }
   },
   mounted () {
+    console.log('this is current swiper instance object', this.swiper);
     this.swiper.slideTo(3, 1000, false)
   }
 
@@ -115,69 +118,4 @@ export default {
   
 <style lang="scss" scoped>
 @import url("../assets/scss/main.scss");
-.main {
-  position: relative;
-  width: 100%;
-  background-color: #f5f5f5;
-
-  .container {
-    position: relative;
-    width: 1226px;
-    margin-left: auto;
-    margin-right: auto;
-
-    .swiper-container {
-      position: relative;
-      width: 1226px;
-      height: 460px;
-
-      img {
-        width: 1226px;
-        height: 460px;
-      }
-      .swiper-warp {
-        position: relative;
-        z-index: 10px;
-
-        .category-item {
-          position: absolute;
-          top: 0;
-          left: 0;
-          z-index: 9999;
-          display: block;
-          float: left;
-          width: 234px;
-          border: 0;
-          padding: 20px 0;
-          font-size: 14px;
-          background-color: rgba(105, 101, 101, 0.6);
-
-          li {
-            width: 234px;
-            height: 42px;
-            line-height: 42px;
-            padding-left: 30px;
-            display: inline-block;
-            position: relative;
-            text-align: start;
-
-            a {
-              color: #fff;
-              cursor: pointer;
-
-              i {
-                position: absolute;
-                right: 20px;
-              }
-            }
-          }
-
-          li:hover {
-            background-color: #ff6700;
-          }
-        }
-      }
-    }
-  }
-}
 </style>
